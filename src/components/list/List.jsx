5cbf5a35d94015037a70fdd9246671218d7f0d8e
Alessandro Marchesin <a.marchesin@epsonemear.com>
2023-04-27 18:01:54 +0200

    Eslint!

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import { ListGroup, Button, Spinner } from "react-bootstrap";
-import "./styles.css";
+import React, { useState } from 'react';
+import { ListGroup, Button, Spinner } from 'react-bootstrap';
+import './styles.css';
 
-const List = () => {
+function List() {
   const [repositories, setRepositories] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchRepositories = async () => {
-    const response = await fetch('https://api.github.com/users/devpass-tech/repos')
+    setIsLoading(true);
+    const response = await fetch('https://api.github.com/users/devpass-tech/repos');
   };
 
   return (
@@ -15,18 +16,19 @@ const List = () => {
       <div className="container">
         <h2 className="title">Devpass Repositories</h2>
 
-        { isLoading ?
-        ( <Spinner/> ) : 
-        ( 
-          <ListGroup className="repositoriesList">
+        {isLoading
+          ? (<Spinner />)
+          : (
+            <ListGroup className="repositoriesList">
 
-          { /* TODO */ }
-          
-          </ListGroup> )}
-      <Button data-testid="button" className="button" variant="primary" onClick={() => fetchRepositories()}>Fetch repositories</Button>
+              {setIsLoading(false)}
+
+            </ListGroup>
+          )}
+        <Button data-testid="button" className="button" variant="primary" onClick={() => fetchRepositories()}>Fetch repositories</Button>
       </div>
     </div>
   );
-};
+}
 
 export default List;
